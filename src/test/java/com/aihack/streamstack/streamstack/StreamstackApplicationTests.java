8f670ca68592bd66e4f1befeede9f23d0b622e90
Andrew Whitehouse <andrew.whitehouse@digital.homeoffice.gov.uk>
2025-04-26 14:11:08 +0100

    Initial template

---
+++ src/test/java/com/aihack/streamstack/streamstack/StreamstackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aihack.streamstack.streamstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
